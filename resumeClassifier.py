7ffc00ea78bd46eff15ff66ed2a4f7e866b2386f
Jmiu <jerrymiu@outlook.com>
2019-01-30 14:51:07 -0500

    adding translation function from index to word

--- resumeClassifier.py
+++ resumeClassifier.py
@@ -13,7 +13,16 @@ from itertools import count
 
 rawData = pd.read_csv('resume_dataset.csv')
 stopWords = set(stopwords.words('english'))
-vocabulary = defaultdict(count(0).next)
+vocabulary = defaultdict(count(1).next)
+maxLength = 0
+
+def getWordFromIndex(wordIndex):
+	if isinstance(wordIndex, str):
+		wordIndex = int(wordIndex)
+	for word, index in vocabulary.items():
+		if wordIndex == index:
+			return word
+	return "Not Found"
 
 # Process data
 for index, row in rawData.iterrows():
@@ -25,10 +34,17 @@ for index, row in rawData.iterrows():
 	
 	# Remove empty tokens, remove stopwords and convert to lowercase
 	tokens = [token.lower() for token in tokens if token and token not in stopWords]
-	
+	maxLength = max(len(tokens), maxLength)
 	# Store each unique word to the vocabulary dictionary
 	for token in tokens:
 		vocabulary[token]
-
+	
 	# Convert the tokens for each resume into a list of integers
-	row[2] = map(lambda token: vocabulary[token], tokens)
+	processedTokens = map(lambda token: str(vocabulary[token]), tokens)
+	rawData.loc[index, 2] = ' '.join(processedTokens)
+
+deciphered = map(getWordFromIndex, rawData.loc[1, 2].split())
+
+lenVocab = len(vocabulary)
+vocabulary["<PAD>"] = 0
+
