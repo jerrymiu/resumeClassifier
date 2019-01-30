7328088bdf6437e1e61ddb72c9cc9a7ff42b1761
Jmiu <jerrymiu@outlook.com>
2019-01-30 11:54:27 -0500

    removed escape sequences and empty tokens

--- resumeClassifier.py
+++ resumeClassifier.py
@@ -8,7 +8,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 rawData = pd.read_csv('resume_dataset.csv')
-print(rawData)
 
-#Convert words to an integer
+# Process data
+for index, row in rawData.iterrows():
+	# Tokenize the text of each resume
+	tokens = row[2].split()
+
+	# Remove escape sequences
+	tokens = [''.join(tokenChar for tokenChar in rawToken if rawToken.isalnum()) for rawToken in tokens]
+	
+	# Remove empty tokens
+	tokens = [token for token in tokens if token]
+	
+	# Convert to lowercase
+
+	# Convert each unique word to a dictionary
+	
 
