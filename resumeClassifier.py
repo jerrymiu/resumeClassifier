c82f81b1363113abf0efc05674a579f9ae0ddc13
Jmiu <jerrymiu@outlook.com>
2019-01-30 12:16:08 -0500

    removed stop words, converted each unique word to an integer

--- resumeClassifier.py
+++ resumeClassifier.py
@@ -6,9 +6,14 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from nltk.corpus import stopwords
+
+from collections import defaultdict
+from itertools import count
 
 rawData = pd.read_csv('resume_dataset.csv')
-
+stopWords = set(stopwords.words('english'))
+vocabulary = defaultdict(count(0).next)
 # Process data
 for index, row in rawData.iterrows():
 	# Tokenize the text of each resume
@@ -17,11 +22,10 @@ for index, row in rawData.iterrows():
 	# Remove escape sequences
 	tokens = [''.join(tokenChar for tokenChar in rawToken if rawToken.isalnum()) for rawToken in tokens]
 	
-	# Remove empty tokens
-	tokens = [token for token in tokens if token]
+	# Remove empty tokens, remove stopwords and convert to lowercase
+	tokens = [token.lower() for token in tokens if token and token not in stopWords]
 	
-	# Convert to lowercase
-
 	# Convert each unique word to a dictionary
-	
+	for token in tokens:
+		vocabulary[token]
 
