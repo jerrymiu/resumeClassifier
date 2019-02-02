7fe4a1ebc4bfca4ff05131d3703d443a680ee05d
Jmiu <jerrymiu@outlook.com>
2019-02-02 17:16:26 -0500

    used keras to pad data set with 0s to form uniform length

--- resumeClassifier.py
+++ resumeClassifier.py
@@ -16,35 +16,30 @@ stopWords = set(stopwords.words('english'))
 vocabulary = defaultdict(count(1).next)
 maxLength = 0
 
-def getWordFromIndex(wordIndex):
-	if isinstance(wordIndex, str):
-		wordIndex = int(wordIndex)
-	for word, index in vocabulary.items():
-		if wordIndex == index:
-			return word
-	return "Not Found"
-
-# Process data
-for index, row in rawData.iterrows():
+def convertWordToIndex(resumeText):
 	# Tokenize the text of each resume
-	tokens = row[2].split()
+	tokens = resumeText.split()
 
 	# Remove escape sequences
 	tokens = [''.join(tokenChar for tokenChar in rawToken if rawToken.isalnum()) for rawToken in tokens]
 	
 	# Remove empty tokens, remove stopwords and convert to lowercase
 	tokens = [token.lower() for token in tokens if token and token not in stopWords]
-	maxLength = max(len(tokens), maxLength)
+	
 	# Store each unique word to the vocabulary dictionary
 	for token in tokens:
 		vocabulary[token]
 	
 	# Convert the tokens for each resume into a list of integers
 	processedTokens = map(lambda token: str(vocabulary[token]), tokens)
-	rawData.loc[index, 2] = ' '.join(processedTokens)
+	return processedTokens
 
-deciphered = map(getWordFromIndex, rawData.loc[1, 2].split())
+category = rawData['Category'].tolist()
+trainData = rawData['Resume'].tolist()
+
+# Process data
+trainData = map(convertWordToIndex, trainData)
+trainData = keras.preprocessing.sequence.pad_sequences(trainData, value=0, padding='post', maxlen=3952)
 
 lenVocab = len(vocabulary)
 vocabulary["<PAD>"] = 0
-
