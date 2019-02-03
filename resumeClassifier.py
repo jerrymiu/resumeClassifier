1351c719f36a12513534c3ff700a17c447657acc
Jmiu <jerrymiu@outlook.com>
2019-02-03 00:04:42 -0500

    prototyped and trained model

--- resumeClassifier.py
+++ resumeClassifier.py
@@ -14,7 +14,7 @@ from itertools import count
 rawData = pd.read_csv('resume_dataset.csv')
 stopWords = set(stopwords.words('english'))
 vocabulary = defaultdict(count(1).next)
-maxLength = 0
+categoryNames = defaultdict(count(0).next)
 
 def convertWordToIndex(resumeText):
 	# Tokenize the text of each resume
@@ -31,11 +31,17 @@ def convertWordToIndex(resumeText):
 		vocabulary[token]
 	
 	# Convert the tokens for each resume into a list of integers
-	processedTokens = map(lambda token: str(vocabulary[token]), tokens)
+	processedTokens = map(lambda token: vocabulary[token], tokens)
 	return processedTokens
 
-category = rawData['Category'].tolist()
+trainLabels = rawData['Category'].tolist()
 trainData = rawData['Resume'].tolist()
+
+# Process Labels
+for trainLabel in trainLabels:
+	categoryNames[trainLabel]
+
+trainLabels = map(lambda trainLabel: categoryNames[trainLabel], trainLabels)
 
 # Process data
 trainData = map(convertWordToIndex, trainData)
@@ -43,3 +49,18 @@ trainData = keras.preprocessing.sequence.pad_sequences(trainData, value=0, paddi
 
 lenVocab = len(vocabulary)
 vocabulary["<PAD>"] = 0
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(lenVocab+1, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(300, activation=tf.nn.relu))
+model.add(keras.layers.Dense(25, activation=tf.nn.sigmoid))
+model.summary()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+npTrainData = np.asarray(trainData, dtype=np.int32)
+npTrainLabels = np.asarray(trainLabels, dtype=np.int32)
+
+history = model.fit(npTrainData, npTrainLabels, epochs=5)
+
+results = model.evaluate(npTrainData, npTrainLabels)
